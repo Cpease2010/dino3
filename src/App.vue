c45d55f30d3102c8b3c92411d4a56c90add24741
Cory Pease <corypease@Corys-MacBook-Pro.local>
2018-08-20 14:26:28 -0600

    finished creating component for jobdetails && added some CSS to App.vue and jobDetails.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <dinoHeader/>
+      <dinoHeader/>
+    <main>
+      <jobDetails/>
+    </main>
     <dinoFooter/>
   </div>
 </template>
@@ -8,15 +11,36 @@
 <script>
 import dinoHeader from './components/dinoHeader.vue'
 import dinoFooter from './components/dinoFooter.vue'
+import jobDetails from './components/jobDetails.vue'
 export default {
   name: 'App',
   components: {
     dinoHeader,
-    dinoFooter
+    dinoFooter,
+    jobDetails
   }
 }
 </script>
 
 <style>
+html {
+  margin: 0;
+  padding: 0;
+}
 
+body {
+  margin: 0 30px 0 30px;
+  padding: 0;
+  font-family: sans-serif;
+  color: #1B997A;
+  display: grid;
+  grid-template-rows: 15% 75% 10%;
+}
+
+main {
+  grid-row: 2/3;
+  width: 70%;
+  margin: 0 auto;
+  padding: 10px;
+}
 </style>
--- src/components/jobDetails.vue
+++ src/components/jobDetails.vue
@@ -1,12 +1,38 @@
 <template>
-  
+<div>
+    <section id="job-details">
+        <h2>Job Details</h2>
+        <h4>{{listing.title}}</h4>
+        <p>{{listing.description}}</p>
+    </section>
+    </div>
 </template>
 
 <script>
-
+export default {
+    name: 'jobDetails',
+    data(){
+        return {
+            listing: [],
+            apiURL: '../../static/listing.json'
+        }
+    },
+    mounted(){
+        fetch(this.apiURL)
+            .then(result => result.json())
+            .then(result => {
+                console.log(result);
+                
+                this.listing = result
+            })
+    }
+}
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+main section {
+  grid-column: 2/3;
+}
 </style>
